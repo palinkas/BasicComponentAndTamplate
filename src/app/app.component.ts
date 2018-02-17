0c0335ba85e87b5290e4b01b7524b4743a0ae798
palinkas <p.patrik25@gmail.com>
2018-02-17 12:37:05 +0100

    way binding and pipe demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular kiraaaaly és nagyon nagy mester leszek belőle';
   events = ['sziget', 'efott', 'fezen', 'balatonsound'];
   show = true;
   klikkeljunke = false;
   inputContent: string;
 
-  toggle(){
+  pipeDemo = [
+      {
+      'id': 1,
+      'name': 'palinka'
+      },
+      {
+      'id': 2,
+      'name': 'Citia'
+      },
+      {
+      'id': 3,
+      'name': 'kisgugya'
+      }
+    ];
+
+  toggle() {
     this.show = !this.show;
     this.inputContent = 'hello, pisti, sanyi, csár';
   }
 
-  demo(ev: MouseEvent){
+  demo(ev: MouseEvent) {
     console.log(ev.screenX);
   }
 }
